46bae5adf1d55b944eaa9fda89a91a6c7f15bd98
mdanilov7 <DanilovMaxim7@yandex.ru>
2016-07-02 20:19:17 +0300

    add query 7

---
+++ query_7.sql
@@ -0,0 +1,11 @@
+select ABONENT_NAME, CNT from
+
+(select ST_ABONENTS.C_NAME as "ABONENT_NAME",
+        count(*) as "CNT"
+from QRY_TYPE, ST_ABONENTS, QRY_QUEUE 
+where QRY_QUEUE.C_QRY_TYPE = QRY_TYPE.ID and 
+      QRY_TYPE.C_AB_REF = ST_ABONENTS.ID 
+group by ST_ABONENTS.C_NAME
+order by CNT desc)
+
+where rownum <= 3;
