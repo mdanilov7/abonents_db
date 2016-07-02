45ba5480ca169c80289e7c94598642cacd49245c
mdanilov7 <DanilovMaxim7@yandex.ru>
2016-07-02 17:11:21 +0300

    added 2-nd query

---
+++ query_2.sql
@@ -0,0 +1,7 @@
+select ST_ABONENTS.C_NAME, count(*)
+from QRY_QUEUE, ST_ABONENTS, QRY_TYPE
+where to_char(C_IN_TIME, 'HH24') >= 9  and
+      to_char(C_IN_TIME, 'HH24') <= 18 and
+      QRY_QUEUE.C_QRY_TYPE = QRY_TYPE.ID and 
+      QRY_TYPE.C_AB_REF = ST_ABONENTS.ID
+group by ST_ABONENTS.C_NAME;
