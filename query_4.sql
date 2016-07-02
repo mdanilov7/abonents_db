32301ec39ef79ec3dd36549600955178a155edc3
mdanilov7 <DanilovMaxim7@yandex.ru>
2016-07-02 19:00:10 +0300

    add 4-th query

---
+++ query_4.sql
@@ -0,0 +1,12 @@
+select ST_ABONENTS.C_NAME as "ABONENT_NAME",
+       case
+        when QRY_QUEUE.C_ST is not null
+                then
+                  (case when QRY_QUEUE.C_ST = 1 then 'OK' else 'FAIL' end)
+                else 'NULL'
+           end as "STATUS",
+       count(*) as "CNT"
+from QRY_QUEUE, QRY_TYPE, ST_ABONENTS
+where QRY_QUEUE.C_QRY_TYPE = QRY_TYPE.ID and 
+      QRY_TYPE.C_AB_REF = ST_ABONENTS.ID
+group by ST_ABONENTS.C_NAME, QRY_QUEUE.C_ST;
