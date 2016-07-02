58cdaaf7803f66c1b4fbfadb21b81c7cacf20ac1
mdanilov7 <DanilovMaxim7@yandex.ru>
2016-07-02 20:17:01 +0300

    add query 6 and modified query 4

--- query_4.sql
+++ query_4.sql
@@ -1,10 +1,10 @@
 select ST_ABONENTS.C_NAME as "ABONENT_NAME",
        case
         when QRY_QUEUE.C_ST is not null
-                then
-                  (case when QRY_QUEUE.C_ST = 1 then 'OK' else 'FAIL' end)
-                else 'NULL'
-           end as "STATUS",
+          then 
+            (case when QRY_QUEUE.C_ST = 1 then 'OK' else  'FAIL' end)
+          else 'NULL'
+       end as "STATUS",
        count(*) as "CNT"
 from QRY_QUEUE, QRY_TYPE, ST_ABONENTS
 where QRY_QUEUE.C_QRY_TYPE = QRY_TYPE.ID and 
